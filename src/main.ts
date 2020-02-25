96eb76d2b3ec22e711eca5754c0d4b32ced5b3ca
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-02-25 11:16:21 +0100

    refactoed the code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as core from '@actions/core'
 import { context } from '@actions/github'
 import { GitHub } from '@actions/github'
-//import octokit from './octokit'
+import { setInstallationAccessToken } from './octokit'
+import octokit from './octokit'
 import { CommittersDetails } from './interface'
 import getCommitters from './graphql'
 import prComment from './prcomment'
@@ -10,43 +11,32 @@ const { request } = require("@octokit/request");
 
 async function run() {
   try {
-
-    const PRIVATE_KEY = core.getInput('private_key')
-    const APP_ID = 55339
-    const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY })
-    const jwt = app.getSignedJsonWebToken();
-    console.log(`The JSON web token is ${jwt}`)
-    const { data } = await request("GET /repos/:owner/:repo/installation", {
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      headers: {
-        authorization: `Bearer ${jwt}`,
-        accept: "application/vnd.github.machine-man-preview+json"
-      }
-    });
-    const installationId = data.id;
-    console.log(`Installation id for ${context.issue.repo} repo and owner ${context.issue.owner} is ${installationId}`)
-    const installationAccessToken = await app.getInstallationAccessToken({
-      installationId
-    });
-    console.log(`Installation Access  token for ${context.issue.repo} repo and owner ${context.issue.owner} is ${installationAccessToken}`)
-    const octokit = new GitHub(installationAccessToken as string)
+    await setInstallationAccessToken()
+    // const PRIVATE_KEY = core.getInput('private_key')
+    // const APP_ID = 55339
+    // const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY })
+    // const jwt = app.getSignedJsonWebToken();
+    // console.log(`The JSON web token is ${jwt}`)
+    // const { data } = await request("GET /repos/:owner/:repo/installation", {
+    //   owner: context.issue.owner,
+    //   repo: context.issue.repo,
+    //   headers: {
+    //     authorization: `Bearer ${jwt}`,
+    //     accept: "application/vnd.github.machine-man-preview+json"
+    //   }
+    // });
+    // const installationId = data.id;
+    // console.log(`Installation id for ${context.issue.repo} repo and owner ${context.issue.owner} is ${installationId}`)
+    // const installationAccessToken = await app.getInstallationAccessToken({
+    //   installationId
+    // });
+    // console.log(`Installation Access  token for ${context.issue.repo} repo and owner ${context.issue.owner} is ${installationAccessToken}`)
     await octokit.issues.create({
       owner: context.repo.owner,
       repo: context.repo.repo,
       title: "first issue from octokit cla lite bot",
       body: "wohooooooooooooooooo it is working"
     })
-
-    await request("POST /repos/:owner/:repo/issues", {
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      headers: {
-        authorization: `token ${installationAccessToken}`,
-        accept: "application/vnd.github.machine-man-preview+json"
-      },
-      title: "My installation’s first issue wohooooo!!"
-    });
 
     const committers = (await getCommitters()) as CommittersDetails[]
     await prComment(committers)
--- src/octokit.ts
+++ src/octokit.ts
@@ -1,11 +1,36 @@
 import { GitHub } from '@actions/github'
-import { App } from "@octokit/app"
 import * as core from '@actions/core'
+import { context } from '@actions/github'
 const pat = core.getInput('pat')
+import { App } from "@octokit/app"
+const { request } = require("@octokit/request");
 
+let octokit
 
+export async function setInstallationAccessToken() {
+    const PRIVATE_KEY = core.getInput('private_key')
+    const APP_ID = 55339
+    const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY })
+    const jwt = app.getSignedJsonWebToken();
+    console.log(`The JSON web token is ${jwt}`)
+    const { data } = await request("GET /repos/:owner/:repo/installation", {
+        owner: context.issue.owner,
+        repo: context.issue.repo,
+        headers: {
+            authorization: `Bearer ${jwt}`,
+            accept: "application/vnd.github.machine-man-preview+json"
+        }
+    });
+    const installationId = data.id;
+    console.log(`Installation id for ${context.issue.repo} repo and owner ${context.issue.owner} is ${installationId}`)
+    const installationAccessToken = await app.getInstallationAccessToken({
+        installationId
+    });
+    console.log(`Installation Access  token for ${context.issue.repo} repo and owner ${context.issue.owner} is ${installationAccessToken}`)
 
-const octokit = new GitHub(pat as string)
-//const octokit = new GitHub(process.env.GITHUB_TOKEN as string)
+    octokit = new GitHub(installationAccessToken as string)
+}
+
+//const octokit = new GitHub(octokit() as string)
 
 export default octokit

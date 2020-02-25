7ec6ddda6b195319001dc2470f2c9d5c0f926587
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-02-25 10:19:54 +0100

    getting installation id

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import * as core from '@actions/core'
 import { context } from '@actions/github'
 import { GitHub } from '@actions/github'
 import octokit from './octokit'
-import { App } from "@octokit/app"
 import { CommittersDetails } from './interface'
 import getCommitters from './graphql'
 import prComment from './prcomment'
+import { App } from "@octokit/app"
+const { request } = require("@octokit/request");
 
 async function run() {
   try {
@@ -14,6 +15,16 @@ async function run() {
     const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY })
     const jwt = app.getSignedJsonWebToken();
     console.log(`The JSON web token is ${jwt}`)
+    const { data } = await request("GET /repos/:owner/:repo/installation", {
+      owner: "hiimbex",
+      repo: "testing-things",
+      headers: {
+        authorization: `Bearer ${jwt}`,
+        accept: "application/vnd.github.machine-man-preview+json"
+      }
+    });
+    const installationId = data.id;
+    console.log(`Installation id is ${installationId}`)
     const committers = (await getCommitters()) as CommittersDetails[]
     await prComment(committers)
 

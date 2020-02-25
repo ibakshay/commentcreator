8598e3d232a5cf556834304e208ba102e2b4bcd6
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-02-25 13:27:49 +0100

    test debug

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,12 +1,12 @@
 import { GitHub } from '@actions/github'
 import { installationAccessToken } from "./main"
 
-export function test() {
-    console.log("installation token in octokit file is " + installationAccessToken)
-}
+// export function test() {
+//     console.log("installation token in octokit file is " + installationAccessToken)
+// }
 
 //octokit = new GitHub(installationAccessToken as string)
 
 const octokit = new GitHub(installationAccessToken as string)
 
-export { octokit } 
+export default octokit  
